f210cb0643f3b7519eb25bbcb8f7768db48f2a6b
assignon <yanick007.dev@gmail.com>
2020-03-07 21:14:52 +0100

    dist folder remove from gitignore

---
+++ frontend/dist/precache-manifest.e01e01a7c36219a57fb3ab9f7a0b60c9.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/static/fonts/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "46cce9b81ebcf42fb654",
+    "url": "/static/css/about.2ff1096f.css"
+  },
+  {
+    "revision": "22993bde2a7ee9345d9e",
+    "url": "/static/css/app.0cdbadc5.css"
+  },
+  {
+    "revision": "22993bde2a7ee9345d9e",
+    "url": "/static/js/app.44bbd85c.js"
+  },
+  {
+    "revision": "1e41bae1a828a327d6a2",
+    "url": "/static/css/chunk-vendors.fb53730a.css"
+  },
+  {
+    "revision": "1e41bae1a828a327d6a2",
+    "url": "/static/js/chunk-vendors.3476a039.js"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/static/fonts/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/static/fonts/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/static/fonts/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/static/fonts/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/static/fonts/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/static/fonts/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/static/fonts/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/static/fonts/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/static/fonts/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/static/fonts/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "2fad4ee0a47f41bc76e21c2baf613182",
+    "url": "/static/img/fa-regular-400.2fad4ee0.svg"
+  },
+  {
+    "revision": "46cce9b81ebcf42fb654",
+    "url": "/static/js/about.a350d53f.js"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/static/fonts/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "f249e44df3044e7b0d665b550569ddf0",
+    "url": "/static/img/fa-brands-400.f249e44d.svg"
+  },
+  {
+    "revision": "fdc155d57b7351ef85b3028ea3cfc048",
+    "url": "/static/img/fa-solid-900.fdc155d5.svg"
+  },
+  {
+    "revision": "6e9c02342f158e86ecdb14496cf71924",
+    "url": "/static/img/prod5.6e9c0234.jpeg"
+  },
+  {
+    "revision": "23782dcb765c7826f230e7a8f2f132b6",
+    "url": "/static/img/prod3.23782dcb.jpeg"
+  },
+  {
+    "revision": "775f9d0044f0e1e96664d7384e90087a",
+    "url": "/static/img/prod2.775f9d00.jpeg"
+  },
+  {
+    "revision": "a9d68f6cb068a756313d6136e12fe189",
+    "url": "/static/img/born.a9d68f6c.svg"
+  },
+  {
+    "revision": "9334012564d6bfcaf2739805f2e644ea",
+    "url": "/static/img/register.93340125.jpg"
+  },
+  {
+    "revision": "1f072a28353e84a180db932ce4ed3348",
+    "url": "/static/img/android-chrome-512x512.1f072a28.png"
+  },
+  {
+    "revision": "ca37fd6aec9b421df25b13d47fdb9637",
+    "url": "/static/img/thanks.ca37fd6a.svg"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/static/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "c40c70a0ebf1c203678cb8f5554e8e6a",
+    "url": "/static/img/prod1.c40c70a0.jpeg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a66b29fe90bb2c5fb2a251ab6e1b82f2",
+    "url": "/index.html"
+  }
+];
